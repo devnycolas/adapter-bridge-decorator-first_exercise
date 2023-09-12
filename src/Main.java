d5d79e0b521c65ce4e68a8db9992884fb47c5957
devnycolas <dev.nycolas@outlook.com>
2023-09-12 16:30:57 -0300

    Add BufferedReader

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         // Cardápio de pizzas disponíveis
         Map<String, Pizza> cardapio = new HashMap<>();
@@ -15,79 +17,87 @@ public class Main {
         int tentativasRestantes = 3;
 
         while (tentativasRestantes > 0) {
-            // Solicitar o nome da pizza ao usuário
-            System.out.print("Digite o nome da pizza: ");
-            final String nomePizza = scanner.nextLine().toLowerCase();
+            try {
+                // Solicitar o nome da pizza ao usuário
+                System.out.print("Digite o nome da pizza: ");
+                final String nomePizza = reader.readLine().toLowerCase();
 
-            // Verificar se a pizza está no cardápio
-            Pizza pizza = cardapio.get(nomePizza);
+                // Verificar se a pizza está no cardápio
+                Pizza pizza = cardapio.get(nomePizza);
 
-            if (pizza == null) {
-                System.out.println("Pizza não encontrada no cardápio. Tentativas restantes: " + (tentativasRestantes - 1));
-                for (String pizzaNome : cardapio.keySet()) {
-                    System.out.println("- " + pizzaNome);
+                if (pizza == null) {
+                    System.out.println("Pizza não encontrada no cardápio. Tentativas restantes: " + (tentativasRestantes - 1));
+                    for (String pizzaNome : cardapio.keySet()) {
+                        System.out.println("- " + pizzaNome);
+                    }
+
+                    if (tentativasRestantes == 1) {
+                        System.out.println("Você esgotou suas tentativas.");
+                        break;
+                    }
+
+                    tentativasRestantes--;
+                    continue;
                 }
 
-                if (tentativasRestantes == 1) {
-                    System.out.println("Você esgotou suas tentativas.");
-                    break;
+                // Solicitar o tamanho da pizza ao usuário
+                System.out.print("Digite o tamanho da pizza (Pequena, Media ou Grande): ");
+                final String tamanhoPizza = reader.readLine().toLowerCase();
+
+                double valorPedido = 0.0; // Valor inicial como zero
+
+                switch (tamanhoPizza) {
+                    case "pequena":
+                        valorPedido = 25.0; // Valor para pizza pequena
+                        break;
+                    case "media":
+                        valorPedido = 35.0; // Valor para pizza média
+                        break;
+                    case "grande":
+                        valorPedido = 50.0; // Valor para pizza grande
+                        break;
+                    default:
+                        System.out.println("Tamanho de pizza inválido.");
+                        return;
                 }
 
-                tentativasRestantes--;
-                continue;
+                // Solicitar o método de pagamento ao usuário
+                System.out.print("Digite o método de pagamento (Dinheiro, Cartao ou Pix): ");
+                String metodoPagamento = reader.readLine().toLowerCase();
+
+                PaymentAdapter pagamento;
+                switch (metodoPagamento) {
+                    case "dinheiro":
+                        pagamento = new PagamentoDinheiro();
+                        break;
+                    case "cartão":
+                        pagamento = new PagamentoCartaoCredito();
+                        break;
+                    case "pix":
+                        pagamento = new PagamentoPix();
+                        break;
+                    default:
+                        System.out.println("Método de pagamento inválido.");
+                        return;
+                }
+
+                // Montar a pizza
+                final String mensagemMontagem = "Pizza de " + nomePizza + " " + tamanhoPizza + " montada.";
+                pizzaMontada(pizza, tamanhoPizza, mensagemMontagem);
+
+                // Efetuar o pagamento com base no valor do pedido
+                pagamento.pagar(valorPedido);
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
 
-            // Solicitar o tamanho da pizza ao usuário
-            System.out.print("Digite o tamanho da pizza (Pequena, Media ou Grande): ");
-            final String tamanhoPizza = scanner.nextLine().toLowerCase();
-
-            double valorPedido = 0.0; // Valor inicial como zero
-
-            switch (tamanhoPizza) {
-                case "pequena":
-                    valorPedido = 20.0; // Valor para pizza pequena
-                    break;
-                case "media":
-                    valorPedido = 35.0; // Valor para pizza média
-                    break;
-                case "grande":
-                    valorPedido = 50.0; // Valor para pizza grande
-                    break;
-                default:
-                    System.out.println("Tamanho de pizza inválido.");
-                    return;
-            }
-
-            // Solicitar o método de pagamento ao usuário
-            System.out.print("Digite o método de pagamento (Dinheiro, Cartao ou Pix): ");
-            String metodoPagamento = scanner.nextLine().toLowerCase();
-
-            PaymentAdapter pagamento;
-            switch (metodoPagamento) {
-                case "dinheiro":
-                    pagamento = new PagamentoDinheiro();
-                    break;
-                case "cartao":
-                    pagamento = new PagamentoCartaoCredito();
-                    break;
-                case "pix":
-                    pagamento = new PagamentoPix();
-                    break;
-                default:
-                    System.out.println("Método de pagamento inválido.");
-                    return;
-            }
-
-            // Montar a pizza
-            final String mensagemMontagem = "Pizza de " + nomePizza + " " + tamanhoPizza + " montada.";
-            pizzaMontada(pizza, tamanhoPizza, mensagemMontagem);
-
-            // Efetuar o pagamento com base no valor do pedido
-            pagamento.pagar(valorPedido);
-
-            // Fechar o scanner
-            scanner.close();
-            break;
+        // Fechar o BufferedReader
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
